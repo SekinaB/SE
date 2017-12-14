70933e6d78a53b4d08205499bbfba9a5d3bba3e7
HooBaeBoo <jaiunpetitcerveau@gmail.com>
2017-12-14 23:20:16 +0100

    Maybe consummer dead

--- Projet_SE/src/jus/poc/prodcons/v1/TestProdCons.java
+++ Projet_SE/src/jus/poc/prodcons/v1/TestProdCons.java
@@ -1,36 +1,77 @@
 package jus.poc.prodcons.v1;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.InvalidPropertiesFormatException;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 import jus.poc.prodcons.*;
 
 public class TestProdCons extends Simulateur {
-	protected int nbProd, nbCons, nbBuffer, tempsMoyenProduction;
-	protected int deviationTempsMoyenProduction, tempsMoyenConsommation;
-	protected int deviationTempsMoyenConsommation, nombreMoyenDeProduction;
-	protected int deviationNombreMoyenDeProduction, nombreMoyenNbExemplaire;
-	protected int deviationNombreMoyenNbExemplaire;
+	private int nbProd;
+	private int nbCons;
+	private int nbBuffer;
+	private int tempsMoyenProduction;
+	private int nombreMoyenDeProduction;
+	private int deviationTempsMoyenProduction;
+	private int deviationNombreMoyenDeProduction;
+	private int tempsMoyenConsommation;
+	private int deviationTempsMoyenConsommation;
+
+	private int nombreMoyenNbExemplaire;
+	private int deviationNombreMoyenNbExemplaire;
+
+	private List<Producteur> listProd;
+	private List<Consommateur> listCons;
 
 	public TestProdCons(Observateur observateur) {
 		super(observateur);
+		listProd = new ArrayList<Producteur>();
+		listCons = new ArrayList<Consommateur>();
 	}
 
 	protected void run() throws Exception {
+		System.out.println("Run");
 		// le corps de votre programme principal
 		String fileName = "options.xml";
-		init("jus/poc/prodcons/options/" + fileName);
+		init("./jus/poc/prodcons/v1/" + fileName);
 		// creer buffer
-		ProdCons buffer = new ProdCons();
-		// creer prod et consomateurs et les demarrer
-		System.out.println(nbProd);
-		System.out.println(nbCons);
-		System.out.println(nbBuffer);
-		System.out.println(tempsMoyenProduction);
-		// gerer la condition de terminaison
+		System.out.println("Prod created");
+		ProdCons buffer = new ProdCons(nbBuffer);
 
+		// creer prod et consomateurs et les demarrer
+		for (int i = 0; i < nbProd; i++) {
+			int nombreDeProduction = Aleatoire.valeur(nombreMoyenDeProduction, deviationNombreMoyenDeProduction);
+			Producteur currentProd = new Producteur(buffer, i, nombreDeProduction, observateur, tempsMoyenProduction,
+					deviationTempsMoyenProduction);
+			listProd.add(i, currentProd);
+			currentProd.start();
+
+		}
+		for (int i = 0; i < nbCons; i++) {
+			Consommateur currentCons = new Consommateur(buffer, i, observateur, tempsMoyenConsommation,
+					deviationTempsMoyenConsommation);
+			listCons.add(currentCons);
+			currentCons.start();
+
+		}
+		// gerer la condition de terminaison
+		for (int i = 0; i < nbProd; i++) {
+			listProd.get(i).join();
+			System.out.println(i + " Producteur dead");
+		}
+		if(buffer.enAttente()==0){
+			for (int i = 0; i < nbCons; i++) {
+				//System.out.println("trying to die ");
+				listCons.get(i).join();
+				System.out.println(i + " Consommateur dead");
+			}
+		}
+		
+			System.out.println(buffer.getcons());
+			System.out.println(buffer.getprod());
 	}
 
 	/**
@@ -61,6 +102,7 @@ public class TestProdCons extends Simulateur {
 
 	public static void main(String[] args) {
 		new TestProdCons(new Observateur()).start();
+		
 	}
 
 }
