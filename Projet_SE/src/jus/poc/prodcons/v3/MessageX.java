8d55effd28a3671d2394839ea9390adf9a99e599
HooBaeBoo <jaiunpetitcerveau@gmail.com>
2017-12-16 13:03:46 +0100

    Objectif 3 not working : Problem with ProductionMessage in Producteur -> run()

---
+++ Projet_SE/src/jus/poc/prodcons/v3/MessageX.java
@@ -0,0 +1,20 @@
+package jus.poc.prodcons.v3;
+
+import jus.poc.prodcons.Message;
+
+public class MessageX implements Message {
+
+	private int identifiantProducteur;
+	private int identifiantMessage;
+	
+	public MessageX(int identifiantMessage, int identifiantProducteur){
+		this.identifiantProducteur = identifiantProducteur;
+		this.identifiantMessage = identifiantMessage;
+	}
+	
+	public String toString(){
+		return "le message " + identifiantMessage + " du Producteur " + identifiantProducteur; 
+	}
+	
+	
+}
---
+++ Projet_SE/src/jus/poc/prodcons/v3/TestProdCons.java
@@ -0,0 +1,70 @@
+package jus.poc.prodcons.v3;
+
+import jus.poc.prodcons.Acteur;
+import jus.poc.prodcons.Aleatoire;
+import jus.poc.prodcons.ControlException;
+import jus.poc.prodcons.Message;
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons._Producteur;
+
+public class Producteur extends Acteur implements _Producteur {
+
+	private int deviationTempsDeTraitement;
+	private int moyenneTempsDeTraitement;
+	private int identification;
+	private int nombreDeMessages;
+	private ProdCons buffer;
+
+	protected Producteur(ProdCons buffer, int identification, int nombreDeMessages, Observateur observateur,
+			int moyenneTempsDeTraitement, int deviationTempsDeTraitement) throws ControlException {
+		super(Acteur.typeProducteur, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
+		this.buffer = buffer;
+		this.identification = identification;
+		this.nombreDeMessages = nombreDeMessages;
+	}
+
+	@Override
+	public int deviationTempsDeTraitement() {
+		return deviationTempsDeTraitement;
+	}
+
+	@Override
+	public int identification() {
+		return identification;
+	}
+
+	@Override
+	public int moyenneTempsDeTraitement() {
+		return moyenneTempsDeTraitement;
+	}
+
+	@Override
+	public int nombreDeMessages() {
+		return nombreDeMessages;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < nombreDeMessages; i++) {
+			try {
+				int productionDelai = Aleatoire.valeur(moyenneTempsDeTraitement, deviationTempsDeTraitement);
+				MessageX messageProd = new MessageX(i, identification);
+				sleep(productionDelai);
+				try{
+					this.observateur.productionMessage(this, messageProd, productionDelai);
+				}
+				catch(Exception e){
+					e.printStackTrace();
+				}
+				buffer.put(this, messageProd);
+				Message message= null;
+				
+				// System.out.println("Le producteur " + this.identification + "
+				// produit le message " + i);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
