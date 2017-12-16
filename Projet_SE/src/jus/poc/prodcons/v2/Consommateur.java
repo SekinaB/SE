d5903a09e8c13997bfab711ae37eda1e1f699268
Sekina <Sekina@PC-Sekina>
2017-12-16 16:24:23 +0100

    Functioning v2

---
+++ Projet_SE/src/jus/poc/prodcons/v2/Consommateur.java
@@ -0,0 +1,61 @@
+package jus.poc.prodcons.v2;
+
+import jus.poc.prodcons.Acteur;
+import jus.poc.prodcons.Aleatoire;
+import jus.poc.prodcons.ControlException;
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons._Consommateur;
+
+public class Consommateur extends Acteur implements _Consommateur {
+
+	private int deviationTempsDeTraitement;
+	private int moyenneTempsDeTraitement;
+	private int identification;
+	private int nombreDeMessages;
+	private ProdCons buffer;
+
+	protected Consommateur(ProdCons buffer, int identification, Observateur observateur, int moyenneTempsDeTraitement,
+			int deviationTempsDeTraitement) throws ControlException {
+		super(Acteur.typeConsommateur, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
+		this.buffer = buffer;
+		this.identification = identification;
+	}
+
+	@Override
+	public int deviationTempsDeTraitement() {
+		return deviationTempsDeTraitement;
+	}
+
+	@Override
+	public int identification() {
+		return identification;
+	}
+
+	@Override
+	public int moyenneTempsDeTraitement() {
+		return moyenneTempsDeTraitement;
+	}
+
+	@Override
+	public int nombreDeMessages() {
+		return nombreDeMessages;
+	}
+
+	@Override
+	public void run() {
+		MessageX val;
+		while ((buffer.enAttente() > 0) || buffer.producteurAlive()) {
+			try {
+				sleep(Aleatoire.valeur(moyenneTempsDeTraitement, deviationTempsDeTraitement));
+				val = (MessageX) buffer.get(this);
+				System.out.println("Le consommateur " + this.identification + " consomme " + val.toString());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (TestProdCons.DEBUG) {
+			System.out.println("Le consommateur " + this.identification + " is dead ");
+		}
+	}
+
+}
---
+++ Projet_SE/src/jus/poc/prodcons/v2/ProdCons.java
@@ -0,0 +1,95 @@
+package jus.poc.prodcons.v2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
+import jus.poc.prodcons.Message;
+import jus.poc.prodcons.Tampon;
+import jus.poc.prodcons._Consommateur;
+import jus.poc.prodcons._Producteur;
+
+public class ProdCons implements Tampon {
+
+	private List<MessageX> buffer;
+	private int tailleMax;
+	private int nbProd;
+	private int nbConsummed = 0;
+	private int nbProduced = 0;
+	Semaphore notFull;
+	Semaphore notEmpty;
+	Semaphore mutex;
+
+	public ProdCons(int nbBuffer, int nbProd) {
+		this.buffer = new ArrayList<MessageX>();
+		this.tailleMax = nbBuffer;
+		this.nbProd = nbProd;
+		notFull = new Semaphore(nbBuffer);
+		notEmpty = new Semaphore(0);
+		mutex = new Semaphore(1);
+	}
+
+	@Override
+	public int enAttente() {
+		return buffer.size();
+	}
+
+	@Override
+	public Message get(_Consommateur arg0) throws Exception, InterruptedException {
+		MessageX message;
+
+		notEmpty.acquire();
+		mutex.acquire();
+
+		message = buffer.remove(0);
+		nbConsummed++;
+
+		mutex.release();
+		notFull.release();
+		return message;
+	}
+
+	@Override
+	public void put(_Producteur arg0, Message arg1) throws Exception, InterruptedException {
+		notFull.acquire();
+		if (TestProdCons.DEBUG) {
+			System.out.println("PUT NOTFULL " + arg0.identification());
+		}
+		mutex.acquire();
+		if (TestProdCons.DEBUG) {
+			System.out.println("PUT MUTEX  " + arg0.identification());
+		}
+		buffer.add(taille(), (MessageX) arg1);
+		nbProduced++;
+		mutex.release();
+		if (TestProdCons.DEBUG) {
+			System.out.println("PUT MUTEX Released " + arg0.identification());
+		}
+		notEmpty.release();
+		if (TestProdCons.DEBUG) {
+			System.out.println("Put FIFO Released by" + arg0.identification());
+		}
+	}
+
+	@Override
+	public int taille() {
+		return buffer.size();
+	}
+
+	public void finProducteur() {
+		this.nbProd--;
+	}
+
+	public boolean producteurAlive() {
+		return !(nbProd == 0);
+	}
+
+	public int getProduced() {
+		return this.nbProduced;
+	}
+
+	public int getConsummed() {
+		return this.nbConsummed;
+	}
+
+}
