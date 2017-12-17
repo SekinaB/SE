1c319f4291a9504fb7d2918cb9216e7c7bebdc71
AminaDB <amina.boucherima@hotmail.com>
2017-12-17 17:22:04 +0100

    Update ProdCons.java

--- Projet_SE/src/jus/poc/prodcons/v2/ProdCons.java
+++ Projet_SE/src/jus/poc/prodcons/v2/ProdCons.java
@@ -1,13 +1,15 @@
-package jus.poc.prodcons.v2;
+package jus.poc.prodcons.v5;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import jus.poc.prodcons.Message;
+import jus.poc.prodcons.Observateur;
 import jus.poc.prodcons.Tampon;
 import jus.poc.prodcons._Consommateur;
 import jus.poc.prodcons._Producteur;
-import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.*;
+import java.util.Date;
 
 public class ProdCons implements Tampon {
 
@@ -15,18 +17,29 @@ public class ProdCons implements Tampon {
 	private int tailleMax;
 	private int consummed = 0;
 	private int produced = 0;
-	Semaphore notFull;
-	Semaphore notEmpty;
-	Semaphore mutex;
-
+	Observateur obs;
+	
+	final Lock lock;
+	final Condition notFull;
+	final Condition notEmpty;
+	
 	public ProdCons(int nbBuffer) {
 		this.buffer = new ArrayList<MessageX>();
 		this.tailleMax = nbBuffer;
-		notFull = new Semaphore(nbBuffer);
-		notEmpty = new Semaphore(0);
-		mutex = new Semaphore(1);
+		
+		lock = new ReentrantLock();
+		notFull = lock.newCondition();
+		notEmpty = lock.newCondition();
 	}
 
+	public ProdCons(int nbBuffer, Observateur obs) {
+		this.buffer = new ArrayList<MessageX>();
+		this.tailleMax = nbBuffer;
+		lock = new ReentrantLock();
+		notFull = lock.newCondition();
+		notEmpty = lock.newCondition();
+		this.obs=obs;
+	}
 	@Override
 	public int enAttente() {
 		return buffer.size();
@@ -34,35 +47,50 @@ public class ProdCons implements Tampon {
 
 	@Override
 	public Message get(_Consommateur arg0) throws Exception, InterruptedException {
+		
+		Date date = new Date();
+		double seconds;
 		MessageX message;
 		
-		notEmpty.acquire();
-		mutex.acquire();
-
-		message = buffer.remove(0);
-		consummed++;
-		
-		System.out.println("RETRAIT :  " + message + " PAR " + arg0.identification());
-		mutex.release();
-		notFull.release();
-		
-		return message;
-
+		lock.lock();
+		try {
+			while (enAttente() == 0) {
+				notEmpty.await();
+			}	
+			message = buffer.remove(0);
+			seconds = date.getTime();
+			consummed++;
+			obs.retraitMessage(arg0, message);
+			notFull.signal();
+			
+			System.out.println("RETRAIT :  " + message + " PAR " + arg0.identification() + " AT " + seconds);
+			return message;
+		} finally {
+			lock.unlock();
+		}	
 	}
 
 	@Override
 	public void put(_Producteur arg0, Message arg1) throws Exception, InterruptedException {
-		notFull.acquire();
-		//System.out.println("PUT NOTFULL " + arg0.identification());
-		mutex.acquire();
-		//System.out.println("PUT MUTEX  " + arg0.identification());
-		buffer.add(taille(), (MessageX) arg1);
-		produced++;
-		System.out.println("DEPOT :  " + arg1.toString() + " PAR " + arg0.identification());
-		mutex.release();
-		//System.out.println("PUT MUTEX Released " + arg0.identification());
-		notEmpty.release();
-		//System.out.println("Put FIFO Released by" + arg0.identification());
+		
+		Date date = new Date();
+		double seconds;
+		lock.lock();
+		
+		try {
+			while(taille() >= tailleMax) {
+				notFull.await();
+			}
+			buffer.add(taille(), (MessageX) arg1);
+			seconds = date.getTime();
+			produced++;
+			obs.depotMessage(arg0, arg1);
+			System.out.println("DEPOT :  " + arg1.toString() + " PAR " + arg0.identification() + " AT " + seconds);
+		
+			notEmpty.signal();
+		} finally {
+			lock.unlock();
+		}	
 	}
 
 	@Override
